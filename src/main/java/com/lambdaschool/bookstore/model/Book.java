dd1611470166228ade3ca36aa6627b0570312397
Derrick Mei <derrick.mei.168@gmail.com>
2019-03-01 15:36:04 -0600

    created: models
    Author
    Book
    Section
    User - copied

---
+++ src/main/java/com/lambdaschool/bookstore/model/Book.java
@@ -0,0 +1,101 @@
+package com.lambdaschool.bookstore.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "book") //not necessary but I like being consistent.  It's also weird bc table name is not plural of book
+public class Book
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long bookid;
+
+    private String title;
+
+    private String isbn;
+
+    @Column(nullable = true)
+    private int copy;
+
+    //ManyToOne books to section
+    @ManyToOne
+    @JoinColumn(name = "sectionid", nullable = true) //should nullable be true?  can we have a book without a section?
+    @JsonIgnoreProperties("books")
+    private Section section;
+
+    //ManyToMany books to authors -> book owns table
+    @ManyToMany
+    @JoinTable(name = "wrote",
+            joinColumns = {@JoinColumn(name = "bookid")},
+            inverseJoinColumns = {@JoinColumn(name = "authorid")})
+    @JsonIgnoreProperties("books")
+    private Set<Author> authors = new HashSet<>();
+
+    public Book()
+    {
+    }
+
+    public long getBookid()
+    {
+        return bookid;
+    }
+
+    public void setBookid(long bookid)
+    {
+        this.bookid = bookid;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public String getIsbn()
+    {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn)
+    {
+        this.isbn = isbn;
+    }
+
+    public int getCopy()
+    {
+        return copy;
+    }
+
+    public void setCopy(int copy)
+    {
+        this.copy = copy;
+    }
+
+    public Section getSection()
+    {
+        return section;
+    }
+
+    public void setSection(Section section)
+    {
+        this.section = section;
+    }
+
+    public Set<Author> getAuthors()
+    {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors)
+    {
+        this.authors = authors;
+    }
+}
---
+++ src/main/java/com/lambdaschool/bookstore/model/Section.java
@@ -0,0 +1,56 @@
+package com.lambdaschool.bookstore.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "section")
+public class Section
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long sectionid;
+
+    private String name;
+
+    //OneToMany to books
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "section")  //should we have cascade? or just detach? basically should column in book be nullable
+    @JsonIgnoreProperties("section")
+    private Set<Book> books;
+
+    public Section()
+    {
+    }
+
+    public long getSectionid()
+    {
+        return sectionid;
+    }
+
+    public void setSectionid(long sectionid)
+    {
+        this.sectionid = sectionid;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public Set<Book> getBooks()
+    {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books)
+    {
+        this.books = books;
+    }
+}
